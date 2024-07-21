9f25425daa5e6410810252f479b68b15442e8fbf
asus <asus@DESKTOP-OTTG4TB>
2024-07-21 16:58:09 +0300

    Add project files.

---
+++ IfElsePratik/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace IfElseDegiskenler 
+{
+    class program 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen Bir Sayı Giriniz:");
+            int Sayı = Convert.ToInt32 (Console.ReadLine());
+
+            //İlk olarak bir sayı aldım ve sayısal değeri hakkında çıktı verileceğinden int'e dönüştürdüm.
+
+            if (Sayı > 10)
+            {
+                Console.WriteLine("Girilen Sayı 10'dan Büyüktür.");
+            }
+
+            //İlk aşamada sayının 10'dan büyük olma durumunu inceledim.
+
+            else if (Sayı ==10)
+            {
+                Console.WriteLine("Girilen Sayı 10'dur.");
+            }
+
+            //İkinci aşamada sayının 10'a eşit olma durumunu inceledim.
+
+            else
+            {
+                Console.WriteLine("Girilen Sayı 10'dan Küçüktür.");
+            }
+
+            //Son aşamada sayının 10'dan küçük olma durumunu inceledim.
+
+
+            //Ayrı bir kontrol mekanizması ile girilen sayının çift olup olmadığını kontrol ettim. Bunu 2'ye bölümünden kalanın sıfır olma durumuna göre şartladım.
+
+            if (Sayı % 2 == 0)
+            {
+                Console.WriteLine("Girilen Sayı Çifttir.");
+
+            }
+            else
+            {
+                Console.WriteLine("Girilen Sayı Tektir.");
+            }
+            // Son aşamada çalıştırıldığında girilen sayının 10'dan büyük-küçük veya eşit olma durumuna ve çift olma durumuna ilişkin iki adet çıktı alınır hale geldi.
+    {
+    }
+         
+
+
+
+        }
+    }
+}
